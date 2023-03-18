f8facd7b1e7acd3272dfeb2a21e2be4db85d04d3
Matecitos <31082992+Kevinmoth@users.noreply.github.com>
2023-03-18 18:42:32 -0300

    Add files via upload

---
+++ Script(Sin_UI).py
@@ -0,0 +1,14 @@
+from PIL import Image, ImageSequence
+
+size = 320, 240  # Tamaño de salida
+im = Image.open("archivo_de_entrada.gif")
+frames = ImageSequence.Iterator(im)
+def thumbnails(frames):
+    for frame in frames:
+        thumbnail = frame.copy()
+        thumbnail.thumbnail(size, Image.ANTIALIAS)
+        yield thumbnail
+frames = thumbnails(frames)
+om = next(frames) 
+om.info = im.info 
+om.save("Archivo_de_salida.gif", save_all=True, append_images=list(frames))
